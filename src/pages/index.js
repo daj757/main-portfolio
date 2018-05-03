5e4ba5746a2fafd192da8ea6ddc99b0f54b3ae0e
Danny <DJ@MacBook-Pro.local>
2018-05-03 02:04:14 -0600

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
-import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react'
 import Landing from '../components/landing'
 import Header from '../components/header'
